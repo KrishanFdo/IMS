8f31f0cb596a9551b6fc713955812ff1ac414838
KrishanFdo <krishanfernando129@gmail.com>
2023-07-30 10:33:25 +0530

    user model created (empty files)

--- IMS/database/migrations/2023_07_25_034737_create_registers_table.php
+++ IMS/database/migrations/2023_07_25_034737_create_registers_table.php
@@ -13,15 +13,15 @@ return new class extends Migration
     {
         Schema::create('registers', function (Blueprint $table) {
             $table->integer('r_id')->autoIncrement();
-            $table->string('r_fname',15);
+            $table->string('r_fname',50);
             $table->string('r_lname',50);
             $table->string('r_year',4);
             $table->string('r_mobile',14);
             $table->string('r_scnum',15)->unique();
             $table->string('r_email',50)->unique();
-            $table->string('r_workplace',25);
+            $table->string('r_workplace',50);
             $table->string('r_role',5);
-            $table->string('r_position',50);
+            $table->string('r_position',100);
             $table->string('r_imgpath')->nullable();
             $table->timestamps();
 
